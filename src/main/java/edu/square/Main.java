c8109936be93b3aae74b43538d34c23efe2d2dda
Square <2499532412@qq.com>
2023-10-09 11:05:05 +0800

    Test Square's Branch

--- src/main/java/edu/square/Main.java
+++ src/main/java/edu/square/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("HELLO PDC TEAM!");
-
+        System.out.println("This is Square's Branch");
     }
 }
