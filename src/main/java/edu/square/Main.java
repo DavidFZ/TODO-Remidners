8870191158de12259ec59127c65cb787c7554788
Square <2499532412@qq.com>
2023-10-09 10:43:15 +0800

    HELLO PDC TEAM

--- src/main/java/edu/square/Main.java
+++ src/main/java/edu/square/Main.java
@@ -3,8 +3,7 @@ package edu.square;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("HELLO PDC TEAM!");
 
-        System.out.println("111");
     }
 }
