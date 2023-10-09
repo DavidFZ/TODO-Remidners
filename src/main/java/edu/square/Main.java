639a036571bdf11cf982bad6ca1626a55df7894a
Yao <1052471877@qq.com>
2023-10-09 10:25:04 +0800

    1

--- src/main/java/edu/square/Main.java
+++ src/main/java/edu/square/Main.java
@@ -2,6 +2,9 @@ package edu.square;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        System.out.println("111");
     }
 }
