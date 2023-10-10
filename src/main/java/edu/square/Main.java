ec821d65d205921776d529e56477530c858a1f14
Square <2499532412@qq.com>
2023-10-10 14:35:08 +0800

    add main ui designer

--- src/main/java/edu/square/Main.java
+++ src/main/java/edu/square/Main.java
@@ -1,11 +1,31 @@
 package edu.square;
 
+import edu.square.UIUtils.JFrameFactory;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        JFrame frame = JFrameFactory.getDefaultJFrame("TODO LIST");
+        frame.setVisible(true);
 
-        System.out.println("111");
-        //111
+        JPanel panel = new JPanel(new BorderLayout());
+
+        JPanel listPanel = new JPanel();
+        Label label = new Label("TODO LIST");
+        listPanel.add(label);
+        JPanel todoPanel = new JPanel();
+        todoPanel.setLayout(new BoxLayout(todoPanel, BoxLayout.Y_AXIS));
+
+        panel.add(listPanel, BorderLayout.WEST);
+        panel.add(todoPanel, BorderLayout.CENTER);
+
+
+
+        frame.add(panel);
+
+
     }
 }
