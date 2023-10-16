da2c5175e5cbae805d4a032b0d5105b2ef250103
Square <2499532412@qq.com>
2023-10-16 23:23:24 +0800

    1.implement query group item (flagged - important for instance) 2.create encapsulated class

--- src/main/java/edu/square/views/component/LeftSideView.java
+++ src/main/java/edu/square/views/component/LeftSideView.java
@@ -1,26 +1,31 @@
 package edu.square.views.component;
 
+import edu.square.entity.Reminder;
 import edu.square.utils.UIUtils.JFrameAttribute;
 import edu.square.utils.UIUtils.JFrameFactory;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
+
+import static edu.square.model.ReminderModel.queryReminderByEmergencyStatus;
+import static edu.square.utils.UIUtils.FontUtil.getBoldFont;
 
 public class LeftSideView {
     public static void main(String[] args) {
         //build a main frame
-        JFrame leftMainFrame = JFrameFactory.buildJFrame(JFrameAttribute.getDefaultAttribute(0.2,""));
-        leftMainFrame.setSize((int)(leftMainFrame.getWidth()),(int)(4*leftMainFrame.getWidth()));
+        JFrame leftMainFrame = JFrameFactory.buildJFrame(JFrameAttribute.getDefaultAttribute(0.2, ""));
+        leftMainFrame.setSize((int) (leftMainFrame.getWidth()), (int) (4 * leftMainFrame.getWidth()));
         leftMainFrame.setLayout(new FlowLayout(FlowLayout.LEFT));
         //edit panel
         JPanel editPanel;
         JButton editButton;
         {
             editPanel = new JPanel();
-            editPanel.setPreferredSize(new Dimension((int)(0.95*leftMainFrame.getWidth()),50));
+            editPanel.setPreferredSize(new Dimension((int) (0.95 * leftMainFrame.getWidth()), 50));
             editPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
             editButton = new JButton("EDIT");
-            editButton.setPreferredSize(new Dimension(60,40));
+            editButton.setPreferredSize(new Dimension(60, 40));
             editButton.setFocusPainted(false);
             //transparent
             editButton.setContentAreaFilled(false);
@@ -34,7 +39,7 @@ public class LeftSideView {
         JTextField searchText;
         {
             searchPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-            searchPanel.setPreferredSize(new Dimension((int)(0.98*leftMainFrame.getWidth()),60));
+            searchPanel.setPreferredSize(new Dimension((int) (0.98 * leftMainFrame.getWidth()), 60));
             searchTips = new JLabel("search");
             Font font2 = new Font("宋体", Font.BOLD, (int) (0.04 * leftMainFrame.getWidth()));
             searchTips.setFont(font2);
@@ -66,30 +71,30 @@ public class LeftSideView {
         JPanel flaggedPanel;
         JButton searchFlaggedItem;
         JTextField numberOfFlagged;
+        JLabel stringNumberOfFlagged;
         JLabel flaggedItemLabel;
 
-        Font fnTitle = new Font("宋体",Font.BOLD,(int)(0.05*leftMainFrame.getWidth()));
-        Font fnButton = new Font("宋体",Font.BOLD,(int)(0.03*leftMainFrame.getWidth()));
-
+        Font fnTitle = new Font("宋体", Font.BOLD, (int) (0.05 * leftMainFrame.getWidth()));
+        Font fnButton = new Font("宋体", Font.BOLD, (int) (0.03 * leftMainFrame.getWidth()));
 
 
         {
             itemManager = new JPanel(new FlowLayout());
-            itemManager.setPreferredSize(new Dimension((int)(1* leftMainFrame.getWidth()),(int)(1.4 * leftMainFrame.getWidth())));
+            itemManager.setPreferredSize(new Dimension((int) (1 * leftMainFrame.getWidth()), (int) (1.4 * leftMainFrame.getWidth())));
             //itemManager.setBackground(Color.WHITE);
             //four blocks of items
 
             //today
             todayPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
             todayPanel.setBackground(Color.WHITE);
-            todayPanel.setPreferredSize(new Dimension((int)(0.45 * leftMainFrame.getWidth()),(int)(0.45 * leftMainFrame.getWidth())));
+            todayPanel.setPreferredSize(new Dimension((int) (0.45 * leftMainFrame.getWidth()), (int) (0.45 * leftMainFrame.getWidth())));
             todayItemLabel = new JLabel("Today");
             todayItemLabel.setFont(fnTitle);
 
             numberOfTodayItem = new JTextField(3);
 
             Panel meanlessT = new Panel();
-            meanlessT.setPreferredSize(new Dimension((int)(0.45 * leftMainFrame.getWidth()),(int)(0.2 * leftMainFrame.getWidth())));
+            meanlessT.setPreferredSize(new Dimension((int) (0.45 * leftMainFrame.getWidth()), (int) (0.2 * leftMainFrame.getWidth())));
 
             searchTodayButton = new JButton("search");
             searchTodayButton.setFont(fnButton);
@@ -103,12 +108,12 @@ public class LeftSideView {
             //completed
             completedPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
             completedPanel.setBackground(Color.WHITE);
-            completedPanel.setPreferredSize(new Dimension((int)(0.45 * leftMainFrame.getWidth()),(int)(0.45 * leftMainFrame.getWidth())));
+            completedPanel.setPreferredSize(new Dimension((int) (0.45 * leftMainFrame.getWidth()), (int) (0.45 * leftMainFrame.getWidth())));
             completedItemLabel = new JLabel("Completed");
             completedItemLabel.setFont(fnTitle);
 
             Panel meanlessC = new Panel();
-            meanlessC.setPreferredSize(new Dimension((int)(0.45 * leftMainFrame.getWidth()),(int)(0.2 * leftMainFrame.getWidth())));
+            meanlessC.setPreferredSize(new Dimension((int) (0.45 * leftMainFrame.getWidth()), (int) (0.2 * leftMainFrame.getWidth())));
 
             numberOfCompletedItem = new JTextField(3);
 
@@ -123,12 +128,12 @@ public class LeftSideView {
             //All
             allPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
             allPanel.setBackground(Color.WHITE);
-            allPanel.setPreferredSize(new Dimension((int)(0.45 * leftMainFrame.getWidth()),(int)(0.45 * leftMainFrame.getWidth())));
+            allPanel.setPreferredSize(new Dimension((int) (0.45 * leftMainFrame.getWidth()), (int) (0.45 * leftMainFrame.getWidth())));
             allItemLabel = new JLabel("All");
             allItemLabel.setFont(fnTitle);
 
             Panel meanlessA = new Panel();
-            meanlessA.setPreferredSize(new Dimension((int)(0.45 * leftMainFrame.getWidth()),(int)(0.2 * leftMainFrame.getWidth())));
+            meanlessA.setPreferredSize(new Dimension((int) (0.45 * leftMainFrame.getWidth()), (int) (0.2 * leftMainFrame.getWidth())));
 
             numberOfAllItem = new JTextField(3);
 
@@ -143,25 +148,28 @@ public class LeftSideView {
             //flagged
             flaggedPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
             flaggedPanel.setBackground(Color.WHITE);
-            flaggedPanel.setPreferredSize(new Dimension((int)(0.45 * leftMainFrame.getWidth()),(int)(0.45 * leftMainFrame.getWidth())));
+            flaggedPanel.setPreferredSize(new Dimension((int) (0.45 * leftMainFrame.getWidth()), (int) (0.45 * leftMainFrame.getWidth())));
 
             flaggedItemLabel = new JLabel("Flagged");
             flaggedItemLabel.setFont(fnTitle);
 
             numberOfFlagged = new JTextField(3);
+            //TODO: refactor this
+            List<Reminder> flaggedList = queryReminderByEmergencyStatus(true);
+            stringNumberOfFlagged = new JLabel(String.valueOf(flaggedList.size()));
+            stringNumberOfFlagged.setFont(getBoldFont(16));
+            stringNumberOfFlagged.setForeground(Color.red);
+
 
             Panel meanlessF = new Panel();
-            meanlessF.setPreferredSize(new Dimension((int)(0.45 * leftMainFrame.getWidth()),(int)(0.2 * leftMainFrame.getWidth())));
+            meanlessF.setPreferredSize(new Dimension((int) (0.45 * leftMainFrame.getWidth()), (int) (0.2 * leftMainFrame.getWidth())));
 
             searchFlaggedItem = new JButton("search");
             searchFlaggedItem.setFont(fnButton);
 
             flaggedPanel.add(flaggedItemLabel);
-            flaggedPanel.add(numberOfFlagged);
+            flaggedPanel.add(stringNumberOfFlagged);
             flaggedPanel.add(meanlessF);
-            flaggedPanel.add(searchFlaggedItem);
-
-
 
         }
         itemManager.add(todayPanel);
@@ -171,14 +179,18 @@ public class LeftSideView {
         leftMainFrame.add(itemManager);
 
 
-
-
-
-
-
-
         leftMainFrame.setVisible(true);
 
 
+    }
+
+
+    //TODO: Encapsulate this class
+    public class ListGroupView {
+        private JPanel panel;
+        private JLabel itemNumberOfGroup;
+        private JLabel flaggedItemLabel;
+        //TODO: find icon resource
+        private JLabel icon;
     }
 }
