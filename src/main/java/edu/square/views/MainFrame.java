8dcd745b8efd16ccb1b51082a973b60f6cb58a31
Yao <1052471877@qq.com>
2023-10-21 12:50:29 +0800

    fix LeftSideView,and create MainFrame

--- src/main/java/edu/square/views/MainFrame.java
+++ src/main/java/edu/square/views/MainFrame.java
@@ -2,7 +2,6 @@ package edu.square.views;
 
 import edu.square.utils.UIUtils.JFrameFactory;
 import edu.square.views.component.LeftSideView;
-import edu.square.views.component.TodayPanelManage;
 
 import javax.swing.*;
 import java.awt.*;
@@ -21,11 +20,7 @@ public class MainFrame {
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setLayout(new FlowLayout(FlowLayout.LEFT));
         LeftSideView leftSideView = new LeftSideView(mainFrame);
-
-
-        TodayPanelManage todayPanelManage = new TodayPanelManage(mainFrame);
         mainFrame.add(leftSideView.getLeftPanel());
-        mainFrame.add(todayPanelManage.getTodayPanelManage());
         mainFrame.setVisible(true);
     }
 }
