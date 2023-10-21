4861abd6f442c2f4c05e6c8553757a1f3bd9e168
Yao <1052471877@qq.com>
2023-10-21 12:50:29 +0800

    fix LeftSideView,and create MainFrame

---
+++ src/main/java/edu/square/views/MainFrame.java
@@ -0,0 +1,26 @@
+package edu.square.views;
+
+import edu.square.utils.UIUtils.JFrameFactory;
+import edu.square.views.component.LeftSideView;
+
+import javax.swing.*;
+import java.awt.*;
+import java.sql.SQLException;
+
+import static edu.square.utils.DBUtils.jdbc.DatabaseStatusUtil.forcedInitDB;
+import static edu.square.utils.DevUtils.isDirExist;
+
+public class MainFrame {
+    public static void main(String[] args) throws SQLException {
+        if (!isDirExist("derbyDB"))
+            forcedInitDB();
+
+
+        JFrame mainFrame = JFrameFactory.getDefaultJFrame(.8d, "Schedule");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setLayout(new FlowLayout(FlowLayout.LEFT));
+        LeftSideView leftSideView = new LeftSideView(mainFrame);
+        mainFrame.add(leftSideView.getLeftPanel());
+        mainFrame.setVisible(true);
+    }
+}
