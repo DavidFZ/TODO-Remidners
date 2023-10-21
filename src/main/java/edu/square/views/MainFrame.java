f7a605c61fb132595e7f66b2daf6dede892571e3
Yao <1052471877@qq.com>
2023-10-21 13:41:59 +0800

    change todayFrame to TodayPanelManage

--- src/main/java/edu/square/views/MainFrame.java
+++ src/main/java/edu/square/views/MainFrame.java
@@ -2,6 +2,7 @@ package edu.square.views;
 
 import edu.square.utils.UIUtils.JFrameFactory;
 import edu.square.views.component.LeftSideView;
+import edu.square.views.component.TodayPanelManage;
 
 import javax.swing.*;
 import java.awt.*;
@@ -20,7 +21,11 @@ public class MainFrame {
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setLayout(new FlowLayout(FlowLayout.LEFT));
         LeftSideView leftSideView = new LeftSideView(mainFrame);
+
+
+        TodayPanelManage todayPanelManage = new TodayPanelManage(mainFrame);
         mainFrame.add(leftSideView.getLeftPanel());
+        mainFrame.add(todayPanelManage.getTodayPanelManage());
         mainFrame.setVisible(true);
     }
 }
