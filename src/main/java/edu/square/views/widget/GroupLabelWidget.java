1139eedf1d6cfdedd610122c590222aeecfbf77f
Yao <1052471877@qq.com>
2023-11-08 16:51:10 +0800

    modify GroupLabelWidget

--- src/main/java/edu/square/views/widget/GroupLabelWidget.java
+++ src/main/java/edu/square/views/widget/GroupLabelWidget.java
@@ -9,32 +9,18 @@ import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GroupLabelWidget {
+import static edu.square.utils.UIUtils.ComponentResizeUtil.*;
+
+public class GroupLabelWidget extends MWidget {
     private static final Font DEFAULT_FONT = FontUtil.getBoldFont(20);
-    private final JLabel groupTitleLabel;
-    private final JLabel countLabel;
-    @Getter
-    private JPanel mainPanel;
+    private JLabel groupTitleLabel;
+    private JLabel countLabel;
 
+    private String title;
 
-    public GroupLabelWidget(Dimension dimension, String title) {
-        mainPanel = new JPanel();
-        mainPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
-        countLabel = new JLabel("0");
-
-        int parentFrameWidth = (int) dimension.getWidth();
-        int parentFrameHeight = (int) dimension.getHeight();
-
-        mainPanel.setPreferredSize(new Dimension(parentFrameWidth, (int) (0.1 * parentFrameHeight)));
-        mainPanel.setBackground(Color.red);
-
-        groupTitleLabel = new JLabel(title);
-        groupTitleLabel.setFont(DEFAULT_FONT);
-        groupTitleLabel.setPreferredSize(new Dimension((int) (0.6 * parentFrameWidth), (int) (0.1 * parentFrameHeight)));
-        mainPanel.add(groupTitleLabel);
-
-        countLabel.setFont(DEFAULT_FONT);
-        mainPanel.add(countLabel);
+    public GroupLabelWidget(Dimension parentDimension, Dimension selfDimension, String title) {
+        super(parentDimension, selfDimension);
+        this.title = title;
     }
 
     public void setGroupViewCount(int count) {
@@ -55,21 +41,47 @@ public class GroupLabelWidget {
         mainPanel.repaint();
     }
 
-    public static class ViewBuilder {
-        private Dimension parentDimension;
+    @Override
+    protected void initializeMainPanel() {
+        mainPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        mainPanel.setPreferredSize(selfDimension);
+        mainPanel.setBackground(Color.red);
+    }
 
-        public ViewBuilder(Dimension parentDimension) {
-            this.parentDimension = parentDimension;
+    @Override
+    protected void initializeFonts() {
+
+    }
+
+    @Override
+    protected void initializeJComponents() {
+        groupTitleLabel = new JLabel(title);
+        groupTitleLabel.setFont(DEFAULT_FONT);
+        groupTitleLabel.setBackground(Color.black);
+        groupTitleLabel.setPreferredSize(resizeDimensionWidthScale(selfDimension, 0.6));
+        mainPanel.add(groupTitleLabel);
+
+        countLabel = new JLabel("0");
+        countLabel.setPreferredSize(resizeDimensionWidthScale(selfDimension, 0.2));
+        countLabel.setFont(DEFAULT_FONT);
+        mainPanel.add(countLabel);
+    }
+
+    public static class ViewBuilder {
+        private Dimension selfDimension;
+
+        public ViewBuilder(Dimension selfDimension) {
+            this.selfDimension = selfDimension;
         }
 
         public GroupLabelWidget build(String title) {
-            return new GroupLabelWidget(parentDimension, title);
+            return new GroupLabelWidget(selfDimension, resizeDimensionHeightScale(selfDimension, 0.22), title);//设置高度
         }
 
         public List<GroupLabelWidget> build(List<String> titles) {
             List<GroupLabelWidget> list = new ArrayList<>(titles.size());
             for (String title : titles)
-                list.add(new GroupLabelWidget(parentDimension, title));
+                list.add(new GroupLabelWidget(selfDimension, resizeDimensionHeightScale(selfDimension, 0.22), title));
             return list;
         }
     }
