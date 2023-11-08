34f8e014842fb1eec7acc4c2d5b4ccba4d5aa3d6
Square <2499532412@qq.com>
2023-11-08 18:10:28 +0800

    rebase abstract class of DetailInformationComponentController

--- src/main/java/edu/square/controller/view1/component/DetailInformationComponentController.java
+++ src/main/java/edu/square/controller/view1/component/DetailInformationComponentController.java
@@ -1,18 +1,20 @@
 package edu.square.controller.view1.component;
 
-import edu.square.controller.view.MComponentController;
+import edu.square.controller.MController;
+import edu.square.model.MModel;
+import edu.square.views.component.MComponent;
 import edu.square.views.view1.component.DetailInformationComponentView;
 
-public class DetailInformationComponentController extends MComponentController {
-    private final DetailInformationComponentView detailInformationComponentView;
+public class DetailInformationComponentController extends MController {
 
-    public DetailInformationComponentController(DetailInformationComponentView detailInformationComponentView) {
-        super();
-        this.detailInformationComponentView = detailInformationComponentView;
+    public DetailInformationComponentController(MComponent mComponent, MModel model) {
+        super(mComponent, model);
     }
 
     @Override
-    public void initController() {
+    public void initialize() {
+        DetailInformationComponentView detailInformationComponentView = (DetailInformationComponentView) mComponentView;
+
         detailInformationComponentView.getBackButton().addActionListener(e -> {
             detailInformationComponentView.setVisibility(false);
         });
