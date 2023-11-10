de1bbbcb29afc72977ae97f18eb2606068511716
Square <2499532412@qq.com>
2023-11-10 14:13:23 +0800

    refactor ComboBoxPanelWidget

--- src/main/java/edu/square/views/widget/ComboBoxPanelWidget.java
+++ src/main/java/edu/square/views/widget/ComboBoxPanelWidget.java
@@ -1,5 +1,8 @@
 package edu.square.views.widget;
 
+import edu.square.utils.UIUtils.FontUtil;
+import edu.square.utils.UIUtils.MWidgetTestHelper;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,12 +10,11 @@ import static edu.square.utils.UIUtils.ComponentResizeUtil.resizeDimensionWidthA
 
 public class ComboBoxPanelWidget extends MWidget {
 
-    JComboBox<String> jComboBox;
-
-    String[] options;
-
-    Font font;
-    String title;
+    private final String[] options;
+    private final String title;
+    private Font font;
+    private JComboBox<String> jComboBox;
+    private JLabel titleLabel;
 
     public ComboBoxPanelWidget(Dimension rootFrameDimension, Dimension selfDimension, Font font, String title, String[] options) {
         super(rootFrameDimension, selfDimension);
@@ -20,6 +22,14 @@ public class ComboBoxPanelWidget extends MWidget {
         this.font = font;
         this.title = title;
         addJComponents();
+    }
+
+    public ComboBoxPanelWidget(Dimension rootFrameDimension, Dimension selfDimension, String title, String[] options) {
+        super(rootFrameDimension, selfDimension);
+        this.options = options;
+        this.title = title;
+
+        updateView();
     }
 
 
@@ -30,19 +40,37 @@ public class ComboBoxPanelWidget extends MWidget {
 
     @Override
     protected void initializeFonts() {
-
+        font = FontUtil.getBoldFont(selfDimension, FontUtil.FONT_SIZE_2);
     }
 
     @Override
     protected void initializeJComponents() {
+        //initialize title label
+        titleLabel = new JLabel();
+        titleLabel.setFont(font);
+        titleLabel.setPreferredSize(resizeDimensionWidthAndHeight(selfDimension, 0.3, 0.8));
+        mainPanel.add(titleLabel);
 
+        //initialize jComboBox
+        jComboBox = new JComboBox<String>();
+        jComboBox.setFont(font);
+        jComboBox.setPreferredSize(resizeDimensionWidthAndHeight(selfDimension, 0.6, 0.8));
+        mainPanel.add(jComboBox);
     }
 
+    private void updateView() {
+        titleLabel.setText(title);
+        for (String option : options) {
+            jComboBox.addItem(option);
+        }
+    }
+
+
     public void addJComponents() {
-        JLabel label = new JLabel(title);
-        label.setFont(font);
-        label.setPreferredSize(resizeDimensionWidthAndHeight(selfDimension, 0.3, 0.8));
-        mainPanel.add(label);
+        titleLabel = new JLabel(title);
+        titleLabel.setFont(font);
+        titleLabel.setPreferredSize(resizeDimensionWidthAndHeight(selfDimension, 0.3, 0.8));
+        mainPanel.add(titleLabel);
 
         jComboBox = new JComboBox<String>();
         jComboBox.setFont(font);
@@ -56,5 +84,18 @@ public class ComboBoxPanelWidget extends MWidget {
 
     public JComboBox<String> getjComboBox() {
         return jComboBox;
+    }
+
+    public static void main(String[] args) {
+        MWidgetTestHelper mWidgetTestHelper = new MWidgetTestHelper() {
+            @Override
+            public void initializeMWidget() {
+                String[] years = {
+                        "2023", "2024", "2025", "2026", "2027", "2028", "2029", "2030"
+                };
+                ComboBoxPanelWidget comboBoxPanelWidget = new ComboBoxPanelWidget(jFrame.getSize(), resizeDimensionWidthAndHeight(jFrame.getSize(), 0.4, 0.07), "years:", years);
+                jFrame.add(comboBoxPanelWidget.getMainPanel());
+            }
+        };
     }
 }
