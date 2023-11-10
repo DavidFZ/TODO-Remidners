bfd8fed9f6241ecf50e78ee89d646f9e1d051e0c
Square <2499532412@qq.com>
2023-11-10 18:37:29 +0800

    fix initialize bug of TimeSelectorComponentView

--- src/main/java/edu/square/views/view1/component/TimeSelectorComponentView.java
+++ src/main/java/edu/square/views/view1/component/TimeSelectorComponentView.java
@@ -5,22 +5,18 @@ import edu.square.utils.UIUtils.FontUtil;
 import edu.square.utils.UIUtils.JPanelUtil;
 import edu.square.utils.UIUtils.MComponentTestHelper;
 import edu.square.views.component.MComponent;
+import edu.square.views.view1.widget.BlockPanelWidget;
 import edu.square.views.view1.widget.ComboBoxPanelWidgetView;
 import lombok.Getter;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.List;
+import java.util.Calendar;
 
-import static edu.square.utils.UIUtils.ComponentResizeUtil.resizeDimensionScale;
-import static edu.square.utils.UIUtils.ComponentResizeUtil.resizeDimensionWidthAndHeight;
+import static edu.square.utils.UIUtils.ComponentResizeUtil.*;
 
 
 public class TimeSelectorComponentView extends MComponent {
-    private final List<String> YEARS = AddReminderConfirmFrameModel.getFutureYears(5);
-    private final List<String> MONTHS = AddReminderConfirmFrameModel.getMonths();
-    private final List<String> HOURS = AddReminderConfirmFrameModel.getHours();
-    List<String> days = AddReminderConfirmFrameModel.getDaysInThisMonth();
     @Getter
     private ComboBoxPanelWidgetView yearsComboBoxPanelWidgetView;
     @Getter
@@ -30,11 +26,10 @@ public class TimeSelectorComponentView extends MComponent {
     @Getter
     private ComboBoxPanelWidgetView hoursComboBoxPanelWidgetView;
     private Font font;
+    private JRadioButton flaggedRadio;
 
     public TimeSelectorComponentView(Dimension parentDimension, Dimension selfDimension) {
         super(parentDimension, selfDimension);
-
-        initializeJComponents();
     }
 
     public static void main(String[] args) {
@@ -62,7 +57,7 @@ public class TimeSelectorComponentView extends MComponent {
 
     @Override
     protected void initializeFonts() {
-        Font font = FontUtil.getBoldFont(parentDimension, FontUtil.FONT_SIZE_0);
+        font = FontUtil.getBoldFont(parentDimension, FontUtil.FONT_SIZE_0);
     }
 
     @Override
@@ -73,10 +68,13 @@ public class TimeSelectorComponentView extends MComponent {
 
         Dimension d = resizeDimensionWidthAndHeight(selfDimension, 0.4, 0.1);
 
-        yearsComboBoxPanelWidgetView = new ComboBoxPanelWidgetView(parentDimension, d, "Year", YEARS);
-        monthsComboBoxPanelWidgetView = new ComboBoxPanelWidgetView(parentDimension, d, "Month", MONTHS);
-        datesComboBoxPanelWidgetView = new ComboBoxPanelWidgetView(parentDimension, d, "Date", days);
-        hoursComboBoxPanelWidgetView = new ComboBoxPanelWidgetView(parentDimension, d, "Hour", HOURS);
+        yearsComboBoxPanelWidgetView = new ComboBoxPanelWidgetView(parentDimension, d, "Year", null);
+        monthsComboBoxPanelWidgetView = new ComboBoxPanelWidgetView(parentDimension, d, "Month", null);
+        datesComboBoxPanelWidgetView = new ComboBoxPanelWidgetView(parentDimension, d, "Date", null);
+        hoursComboBoxPanelWidgetView = new ComboBoxPanelWidgetView(parentDimension, d, "Hour", null);
+
+        flaggedRadio = new JRadioButton("Flagged");
+        flaggedRadio.setFont(FontUtil.getBoldFont(parentDimension, FontUtil.FONT_SIZE_2));
 
         JPanel detailMessagePanel = JPanelUtil.getFlowJpanel(FlowLayout.CENTER, selfDimension);
         detailMessagePanel.add(tipsLabel);
@@ -88,13 +86,38 @@ public class TimeSelectorComponentView extends MComponent {
         b.add(hoursComboBoxPanelWidgetView.getMainPanel());
         detailMessagePanel.add(a);
         detailMessagePanel.add(b);
+        BlockPanelWidget blockPanelWidget = new BlockPanelWidget(parentDimension, resizeDimensionHeightScale(parentDimension, 0.05));
+        detailMessagePanel.add(blockPanelWidget.getMainPanel());
+        detailMessagePanel.add(flaggedRadio);
 
         mainPanel.add(detailMessagePanel);
     }
 
     @Override
     protected void initializeView() {
-
+        updateViews();
+        selectDefaultItem();
     }
+
+    private void updateViews() {
+        yearsComboBoxPanelWidgetView.updateOptionsView(AddReminderConfirmFrameModel.getFutureYears(5));
+        monthsComboBoxPanelWidgetView.updateOptionsView(AddReminderConfirmFrameModel.getMonths());
+        datesComboBoxPanelWidgetView.updateOptionsView(AddReminderConfirmFrameModel.getDaysInThisMonth());
+        hoursComboBoxPanelWidgetView.updateOptionsView(AddReminderConfirmFrameModel.getHours());
+    }
+
+    private void selectDefaultItem() {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH) + 1;
+        int date = calendar.get(Calendar.DATE);
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+
+        yearsComboBoxPanelWidgetView.getjComboBox().setSelectedItem(String.valueOf(year));
+        monthsComboBoxPanelWidgetView.getjComboBox().setSelectedItem(String.valueOf(month));
+        datesComboBoxPanelWidgetView.getjComboBox().setSelectedItem(String.valueOf(date));
+        hoursComboBoxPanelWidgetView.getjComboBox().setSelectedItem(String.valueOf(hour));
+    }
+
 
 }
