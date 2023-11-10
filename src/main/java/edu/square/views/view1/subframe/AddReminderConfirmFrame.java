68c9122322ee396637f0696455e23ec10fe406b0
Yao <1052471877@qq.com>
2023-11-10 13:39:18 +0800

    modify AddReminderConfirmFrame

--- src/main/java/edu/square/views/view1/subframe/AddReminderConfirmFrame.java
+++ src/main/java/edu/square/views/view1/subframe/AddReminderConfirmFrame.java
@@ -28,15 +28,15 @@ public class AddReminderConfirmFrame {
     private final ComboBoxPanelWidget hoursComboBoxPanelWidget;
     private JRadioButton emergentRadio;
     String[] years = {
-            "", "2023", "2024", "2025", "2026", "2027", "2028", "2029", "2030"
+             "2023", "2024", "2025", "2026", "2027", "2028", "2029", "2030"
     };
     String[] months = {
-            "", "1", "2", "3", "4",
+             "1", "2", "3", "4",
             "5", "6", "7", "8",
             "9", "10", "11", "12"
     };
     String[] days = {
-            "", "1", "2", "3", "4",
+             "1", "2", "3", "4",
             "5", "6", "7", "8",
             "9", "10", "11", "12"
     };
