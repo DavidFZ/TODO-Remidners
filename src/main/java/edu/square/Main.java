da3e5860f96e13feec6c05b0bf2c6eb0235e6e93
Square <2499532412@qq.com>
2023-11-10 23:23:56 +0800

    modify ViewBarComponent

--- src/main/java/edu/square/Main.java
+++ src/main/java/edu/square/Main.java
@@ -1,9 +1,9 @@
 package edu.square;
 
+import edu.square.controller.ViewsBarComponentController;
 import edu.square.controller.view1.ListController;
 import edu.square.utils.UIUtils.JFrameFactory;
-import edu.square.views.ViewsBarComponent;
-import edu.square.views.view.MyView;
+import edu.square.views.ViewsBarComponentView;
 import edu.square.views.view1.view.ListView;
 
 import javax.swing.*;
@@ -19,19 +19,29 @@ public class Main {
         if (!isDirExist("derbyDB"))
             forcedInitDB();
 
+        //main frame
         JFrame mainFrame = JFrameFactory.getDefaultJFrame(.8d, "TODO");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setLayout(new FlowLayout(FlowLayout.LEFT));
 
-        MyView myView = new MyView(mainFrame, mainFrame.getSize());
-        ViewsBarComponent viewsBarComponent = new ViewsBarComponent(myView, resizeDimensionWidthScale(mainFrame.getSize(), 0.1));
-        viewsBarComponent.getMainPanel().setLayout(new FlowLayout(FlowLayout.CENTER,0,0));
+        //bar view
+        ViewsBarComponentView viewsBarComponentView = new ViewsBarComponentView(mainFrame.getSize(), resizeDimensionWidthScale(mainFrame.getSize(), 0.1));
+        viewsBarComponentView.getMainPanel().setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
+        mainFrame.add(viewsBarComponentView.getMainPanel());
 
+        //list view
         ListView listView = new ListView(mainFrame, resizeDimensionWidthScale(mainFrame, 0.9));
         ListController listController = new ListController(listView);
         JPanel listPanel = listView.getMainPanel();
         listPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
         mainFrame.add(listView.getMainPanel());
+
+
+
+        Dimension barDimension = resizeDimensionWidthScale(mainFrame.getSize(), 0.1);
+        ViewsBarComponentController viewsBarComponentController = new ViewsBarComponentController(mainFrame.getSize(), barDimension, listView);
+
         mainFrame.setVisible(true);
+
     }
 }
--- src/main/java/edu/square/controller/ViewsBarComponentController.java
+++ src/main/java/edu/square/controller/ViewsBarComponentController.java
@@ -1,23 +1,66 @@
 package edu.square.controller;
 
 
-import edu.square.model.view1.ListModel;
-import edu.square.views.view1.view.ListView;
+import edu.square.views.ViewsBarComponentView;
+import edu.square.views.view.MyView;
+import edu.square.views.view1.widget.LabelPanelWidget;
+
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.List;
 
 public class ViewsBarComponentController {
-    private final ListView listView;
-    private final ListModel listModel;
+    private static final String[] viewsTitles = {"TODO", "Calender", "TomatoTimer"};
+    private final List<MyView> viewList;
+    public ViewsBarComponentView viewsBarComponentView;
+    private int selectedViewIndex = 0;
 
-    public ViewsBarComponentController(ListView listView) {
-        this.listView = listView;
-        this.listModel = new ListModel();
+    public ViewsBarComponentController(Dimension rootFrameDimension, Dimension selfDimension, MyView... myView) {
+        assert myView != null && myView.length > 0;
+        assert myView.length == viewsTitles.length;
 
-        initialize();
+        //initialize
+        viewsBarComponentView = new ViewsBarComponentView(rootFrameDimension, selfDimension, viewsTitles);
+        viewList = List.of(myView);
+
+        //set default selected view
+        List<LabelPanelWidget> list = viewsBarComponentView.getLabelPanelWidgets();
+        list.get(0).setBeSelected();
+
+
+        for (int i = 0; i < myView.length; i++) {
+            int finalI = i;
+            list.get(i).getMainPanel().addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    super.mouseClicked(e);
+                    if (selectedViewIndex == finalI)
+                        return;
+
+                    setLabelWidgetViewSelection(finalI);
+                    setOtherIndexMyViewInVisible(finalI);
+                    selectedViewIndex = finalI;
+                }
+            });
+        }
+
     }
-    private void initialize(){
 
+    private void setOtherIndexMyViewInVisible(int index) {
+        for (int i = 0; i < viewList.size(); i++) {
+            if (i != index)
+                viewList.get(i).getMainPanel().setVisible(false);
+        }
+    }
 
-
-
+    private void setLabelWidgetViewSelection(int beSelectedIndex) {
+        List<LabelPanelWidget> list = viewsBarComponentView.getLabelPanelWidgets();
+        for (int i = 0; i < list.size(); i++) {
+            if (i == beSelectedIndex)
+                list.get(i).setBeSelected();
+            else
+                list.get(i).setNotSelected();
+        }
     }
 }
