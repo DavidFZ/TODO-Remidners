6e2b833ae22201a7c1fcb43e0f9ec10e5d28e176
oliver <3082239309@qq.com>
2023-11-10 14:35:46 +0800

    add controller of calendar

---
+++ src/main/java/edu/square/views/view2/view/CalendarDayPanel.java
@@ -0,0 +1,30 @@
+package edu.square.views.view2.view;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+//每个月中每一天的panel
+public class CalendarDayPanel {
+    JPanel dayPanel = new JPanel();
+    JLabel dayLabel = new JLabel();
+    JTextArea dayTextArea = new JTextArea();
+
+    CalendarDayPanel(int size, String s) {
+        dayPanel.setPreferredSize(new Dimension(size, (int) (0.8 * size)));
+        dayPanel.setBackground(Color.WHITE);
+        dayLabel.setText(s);
+        dayLabel.setFont(new Font("宋体", Font.BOLD, (int) (0.2 * size)));
+        dayPanel.setLayout(new BorderLayout());
+        dayPanel.add(dayLabel, BorderLayout.NORTH);
+        dayTextArea.setLineWrap(true);
+        dayTextArea.setBorder(null);
+        dayPanel.add(dayTextArea, BorderLayout.CENTER);
+
+        dayPanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                ;
+            }
+        });
+    }
+}
