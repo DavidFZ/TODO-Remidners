e2068293f4ad55245cdcceb533a127293a8b3414
Yao <1052471877@qq.com>
2023-11-10 12:43:54 +0800

    modify ComboBoxPanelView

--- src/main/java/edu/square/views/widget/ComboBoxPanelView.java
+++ src/main/java/edu/square/views/widget/ComboBoxPanelView.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 
 import static edu.square.utils.UIUtils.ComponentResizeUtil.resizeDimensionHeightScale;
+import static edu.square.utils.UIUtils.ComponentResizeUtil.resizeDimensionWidthAndHeight;
 
 public class ComboBoxPanelView extends MWidget {
 
@@ -11,29 +12,21 @@ public class ComboBoxPanelView extends MWidget {
 
     String[] options;
 
-    public ComboBoxPanelView(Dimension rootFrameDimension,Dimension dimension, double scalingRatio, Font font, String[] options, Color background) {
-        super(rootFrameDimension,resizeDimensionHeightScale(dimension,scalingRatio));
-        selfDimension = resizeDimensionHeightScale(dimension, scalingRatio);
+    Font font;
+    String title;
+
+    public ComboBoxPanelView(Dimension rootFrameDimension, Dimension selfDimension, Font font, String title, String[] options) {
+        super(rootFrameDimension, selfDimension);
         this.options = options;
-
-        mainPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
-        mainPanel.setPreferredSize(selfDimension);
-        mainPanel.setBackground(background);
-
-        jComboBox = new JComboBox<String>();
-        jComboBox.setFont(font);
-        for (String option : options) {
-            jComboBox.addItem(option);
-        }
-
-        jComboBox.setPreferredSize(new Dimension((int) (0.7 * dimension.width), (int) (0.8 * scalingRatio * dimension.height)));
-        mainPanel.add(jComboBox);
+        this.font = font;
+        this.title = title;
+        addJComponents();
     }
 
 
     @Override
     protected void initializeMainPanel() {
-
+        mainPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
     }
 
     @Override
@@ -44,5 +37,21 @@ public class ComboBoxPanelView extends MWidget {
     @Override
     protected void initializeJComponents() {
 
+    }
+
+    public void addJComponents() {
+        JLabel label = new JLabel(title);
+        label.setFont(font);
+        label.setPreferredSize(resizeDimensionWidthAndHeight(selfDimension, 0.3, 0.8));
+        mainPanel.add(label);
+
+        jComboBox = new JComboBox<String>();
+        jComboBox.setFont(font);
+        for (String option : options) {
+            jComboBox.addItem(option);
+        }
+
+        jComboBox.setPreferredSize(resizeDimensionWidthAndHeight(selfDimension, 0.6, 0.8));
+        mainPanel.add(jComboBox);
     }
 }
