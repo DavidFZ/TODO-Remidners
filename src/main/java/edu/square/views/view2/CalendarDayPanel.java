a5dea4e9f288cded6fba55374dffbe9e16524267
Square <2499532412@qq.com>
2023-11-11 13:38:22 +0800

    add set text method & listener insert method into CalendarDayPanel

--- src/main/java/edu/square/views/view2/CalendarDayPanel.java
+++ src/main/java/edu/square/views/view2/CalendarDayPanel.java
@@ -1,8 +1,9 @@
 package edu.square.views.view2;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+
 //每个月中每一天的panel
 public class CalendarDayPanel {
     JPanel dayPanel = new JPanel();
@@ -18,19 +19,18 @@ public class CalendarDayPanel {
         dayPanel.add(dayLabel, BorderLayout.NORTH);
 
         JLabel jLabel = new JLabel();
-        jLabel.setForeground(new Color(92,179,204));
+        jLabel.setForeground(new Color(92, 179, 204));
 
         JPanel heightPanel = new JPanel();
-        heightPanel.setPreferredSize(new Dimension(size,((int)(0.15*size))));
-        heightPanel.setBackground(new Color(216,227,231));
+        heightPanel.setPreferredSize(new Dimension(size, ((int) (0.15 * size))));
+        heightPanel.setBackground(new Color(216, 227, 231));
         JPanel mainPanel = new JPanel();
-        mainPanel.setPreferredSize(new Dimension(size,((int)(0.75*size))));
-        mainPanel.setBackground(new Color(216,227,231));
+        mainPanel.setPreferredSize(new Dimension(size, ((int) (0.75 * size))));
+        mainPanel.setBackground(new Color(216, 227, 231));
         jLabel.setFont(new Font("宋体", Font.BOLD, (int) (0.4 * size)));
-        jLabel.setText("11");
 
         JPanel notePanel = new JPanel();
-        notePanel.setBackground(new Color(216,227,231));
+        notePanel.setBackground(new Color(216, 227, 231));
         notePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
         mainPanel.add(jLabel);
         notePanel.add(heightPanel);
@@ -41,12 +41,13 @@ public class CalendarDayPanel {
 //        dayTextArea.setLineWrap(true);
 //        dayTextArea.setBorder(null);
 //        dayPanel.add(dayTextArea, BorderLayout.CENTER);
+    }
 
-        dayPanel.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                ;
-            }
-        });
+    public void addMouseListener(MouseAdapter mouseAdapter) {
+        dayPanel.addMouseListener(mouseAdapter);
+    }
+
+    public void setDayLabel(String s) {
+        dayLabel.setText(s);
     }
 }
